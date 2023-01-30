0e390bf1e2cab2015eb2df519a80de7175976424
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2023-01-30 10:08:19 +0530

    Create mv.c

---
+++ Rakesh/sys_progs/mv.c
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#incldude<unistd.h>
+#include<string.h>
+
+int main(int argc, int *argv[])
+{
+  if (arc != 3 || strcmp(argv[1],argv[2])
+      cerr<<"usage:"<<argv[0]<<"o<old_link> <new_link>\n";
+  else if(link(argv[1],argv[2]) == 0)
+      return unlink(argv[1]);
+  return 1;
+}
